35a7fa0e8aa1fc4a61e3ed44a3efc08954b67f5f
Barbara Michelle <barbaramy_@hotmail.com>
2022-05-09 14:31:24 -0300

    encapsulamento

---
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -0,0 +1,27 @@
+
+public class TestaGetESet {
+
+	public static void main(String[] args) {
+		Conta conta =  new Conta(1337, 24226);
+	
+		System.out.println(conta.getNumero());
+		
+		Cliente paulo = new Cliente();
+		//conta.titular = paulo;
+		paulo.setNome("Paulo Silveira");
+		
+		conta.setTitular(paulo);
+		
+		System.out.println(conta.getTitular().getNome());
+		
+		conta.getTitular().setProfissao("Programador");
+		// ou em duas linhas
+		Cliente titularDaConta = conta.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		System.out.println(titularDaConta);
+		System.out.println(paulo);
+		System.out.println(conta.getTitular());
+		
+	}
+}
