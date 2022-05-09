da5cde280aba6f6b658a3bd009de6ad31215a714
lanyablue <barbaramy_@hotmail.com>
2022-05-09 14:58:15 -0300

    sintaxe

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,14 @@
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+	
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);		
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxo/src/Testalacos2.java
@@ -0,0 +1,15 @@
+
+public class Testalacos2 {
+
+	public static void main(String[] args) {
+		for(int linha = 1; linha <10; linha++) {
+			for (int coluna = 0; coluna <= linha ; coluna++) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
